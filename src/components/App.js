0378a712df80d503e75cd9fc21bf07e88d7c4be7
Illia Kolesnikov <kolesnikov00000@gmail.com>
2018-11-19 11:41:34 +0200

    Skeleton of table with time.

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,75 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-  state = { value: '', open: false }
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
-    handleSearchChange = (e, { value }) => {
-      this.setState({ value });
-    }
+let id = 0;
+function createData(name, calories, fat, carbs, protein, kasha, kefir, ryazhanka, seledka, grudka) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein, kasha, kefir, ryazhanka, seledka, grudka };
+}
+const rows = [
+  createData('Понедельник', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'),
+  createData('Вторник', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'),
+  createData('Среда', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'),
+  createData('Четверг', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'),
+  createData('Пятница', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'),
+];
 
-    handleClose = (value) => {
-      this.setState({ open: value });
-    }
+function SimpleTable(props) {
+  const { classes } = props;
 
-    handleOpen = (value) => {
-      this.setState({ open: value });
-    }
-
-    render() {
-      return (
-      <Grid container >
-        <Grid item xs={12}>
-        <Paper>Initial commitfasfasfasf</Paper>
-        </Grid>
-     </Grid>
-      );
-    }
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableBody>
+          {rows.map((row) => {
+            return (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell numeric>
+                <Button>
+                {row.calories}
+                </Button>
+                </TableCell>
+                <TableCell numeric><Button>{row.fat}</Button></TableCell>
+                <TableCell numeric><Button>{row.carbs}</Button></TableCell>
+                <TableCell numeric><Button>{row.protein}</Button></TableCell>
+                <TableCell numeric><Button>{row.kasha}</Button></TableCell>
+                <TableCell numeric><Button>{row.kefir}</Button></TableCell>
+                <TableCell numeric><Button>{row.ryazhanka}</Button></TableCell>
+                <TableCell numeric><Button>{row.seledka}</Button></TableCell>
+                <TableCell numeric><Button>{row.grudka}</Button></TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
 }
 
-export default App;
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
